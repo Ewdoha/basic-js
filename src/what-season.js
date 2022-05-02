5a3948200e97b0a09573676ee43ff33f00fadf66
Ewdoha <olexiyewdoshchenko@gmail.com>
2022-05-02 20:28:38 +0300

    What-season 20 passing

--- src/what-season.js
+++ src/what-season.js
@@ -12,8 +12,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (!date) {
+    console.log('Unable to determine the time of year!');
+    //return 'Unable to determine the time of year!';
+  } else if (typeof(date) === 'number' || typeof(date) === 'string' || typeof(date) === 'boolean') {
+    console.log(`${'(Error)'} ${'Invalid date!'}`);
+    //return `${'(Error)'} ${'Invalid date!'}`;
+  } else {
+    switch (date.getMonth()) {
+        case 1:
+      console.log("Winter");
+      break;
+        case 2:
+      console.log("Winter");
+      break;
+        case 3:
+      console.log("Spring");
+      break;
+        case 4:
+      console.log("Spring");
+      break;
+        case 5:
+      console.log("Spring");
+      break;
+        case 6:
+      console.log("Summer");
+      break;
+        case 7:
+      console.log("Summer");
+      break;
+        case 8:
+      console.log("Summer");
+      break;
+        case 9:
+      console.log("Autumn");
+      break;
+        case 10:
+      console.log("Autumn");
+      break;
+        case 11:
+      console.log("Autumn");
+      break;
+        case 12:
+      console.log("winter");
+      break;
+    }
+  }
 }
 
 module.exports = {
